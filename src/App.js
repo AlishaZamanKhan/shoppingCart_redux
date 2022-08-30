329503de97f8edc557229b1000f55a6a763e96a4
Alisha Zaman Khan <zamankhanalisha@gmail.com>
2022-08-30 13:30:25 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      <Auth />
-      {/* <Layout /> */}
-    </div>
-  );
+	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    console.log(isLoggedIn);
+    const cartItems = useSelector((state) => state.cart.itemsList);
+    console.log("cartItemList",cartItems);
+	return (
+		<div className="App">
+			{!isLoggedIn && <Auth />}
+			{isLoggedIn &&  <Layout /> }
+		</div>
+	);
 }
 
 export default App;
